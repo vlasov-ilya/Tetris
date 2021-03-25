5d78dc508184882880d0dc2e2a473026adf32748
Ilya Vlasov <ilyavlasov93@gmail.com>
2021-03-25 13:33:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,3 @@ const root = document.querySelector('#root');
 const game = new Game();
 const view = new View(root, 480, 640, 20, 10);
 const controller = new Controller(game, view);
-
-window.game = game;
-window.view = view;
-window.controller = controller;
